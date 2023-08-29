63164239e29e927f3e80c762b659e284254ad0e9
Oliver Marler <103157332+rocklandceo@users.noreply.github.com>
2023-08-28 23:19:45 -0700

    fix commit

--- store-locator-script.js
+++ store-locator-script.js
@@ -1,8 +1,8 @@
     // Mapbox token
     mapboxgl.accessToken = 'pk.eyJ1Ijoicm9ja2xhbmQtY2VvIiwiYSI6ImNsbHI5dHU3aTBrOGY0bG0yb2RrMTd6ZGYifQ.1dMr1yO7WZW3zlQA25H46Q';
 
-        // Constants
-        const GOOGLE_SHEETS_CSV_URL = "https://docs.google.com/spreadsheets/d/1n8emJif1ErvRCM_Mn0UBIUGhYY8RNYdrN4aQaZTSoNY/gviz/tq?tqx=out:csv&sheet=Off-Premise-Accounts";
+    // Constants
+    const GOOGLE_SHEETS_CSV_URL = "https://docs.google.com/spreadsheets/d/1n8emJif1ErvRCM_Mn0UBIUGhYY8RNYdrN4aQaZTSoNY/gviz/tq?tqx=out:csv&sheet=Off-Premise-Accounts";
     
     // Initialize the map
     const map = new mapboxgl.Map({
@@ -282,11 +282,12 @@
             // Hide the default Mapbox attribution and add your custom image
             $('.mapboxgl-ctrl-attrib').hide();
             $('.mapboxgl-ctrl-bottom-right').append('<img src="https://uploads-ssl.webflow.com/63a8b8d4f3ba371ef3eca92c/63a8d6cef3af9936f047f013_SD-Logo_HORIZONTAL_GreenAsset%201.svg" alt="Skyduster Dark Logo" class="custom-map-image">');
-        }).fail(function(jqXHR, textStatus, errorThrown) {
+
+        fail(function(jqXHR, textStatus, errorThrown) {
             console.error("Error fetching data: ", textStatus, errorThrown);
             alert("There was an error fetching location data. Please try again later.");
         });
-    }
+    
 
     function geocodeAddress(address, callback) {
         // Construct the URL for Mapbox's Geocoding API
